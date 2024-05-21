0a905901c67e29828222302258df4ff196907370
isreallee82 <isreallee82@gmail.com>
2024-05-21 04:56:00 +0100

    fix Lond to Bigint

--- packages/web/integrations/core-walletconnect/client.ts
+++ packages/web/integrations/core-walletconnect/client.ts
@@ -34,7 +34,6 @@ import {
 } from "@walletconnect/types";
 import { getSdkError } from "@walletconnect/utils";
 import EventEmitter from "events";
-import Long from "long";
 
 import {
   WCAccount,
@@ -548,7 +547,7 @@ export class WCClient implements WalletClient {
       ) =>
         this.signDirect(chainId, signerAddress, {
           ...signDoc,
-          accountNumber: Long.fromString(signDoc.accountNumber.toString()),
+          accountNumber: BigInt(signDoc.accountNumber),
         }),
     } as OfflineDirectSigner;
   }
